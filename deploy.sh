8470836bf957195eee120530097f38c2fb55f9d2
Steve Womack <steve.womack@temporal.io>
2025-06-19 19:39:10 -0500

    Fixes stuff

--- deploy.sh
+++ deploy.sh
@@ -16,19 +16,17 @@ echo "Tagging and pushing image to ACR..."
 docker tag $APP_NAME $ACR_NAME.azurecr.io/$APP_NAME:latest
 docker push $ACR_NAME.azurecr.io/$APP_NAME:latest
 
+echo "Creating Kubernetes namespace..."
+kubectl create namespace $KUBERNETES_NAMESPACE --dry-run=client -o yaml | kubectl apply -f -
+
 echo "Generating Kubernetes manifests from config..."
 ./generate-k8s-manifests.sh
 
-echo "Creating Kubernetes namespace..."
-kubectl create namespace $KUBERNETES_NAMESPACE
+echo "Applying Secret..."
+apply -f acr-secret.yaml --namespace $KUBERNETES_NAMESPACE
 
 echo "Applying ConfigMap..."
 kubectl apply -f config-map.yaml --namespace $KUBERNETES_NAMESPACE
-
-echo "Creating Kubernetes secret for Temporal API key..."
-kubectl create secret generic temporal-secret \
-    --from-literal=TEMPORAL_API_KEY=$TEMPORAL_API_KEY \
-    --namespace $KUBERNETES_NAMESPACE
 
 echo "Deploying to AKS..."
 kubectl apply -f deployment.yaml --namespace $KUBERNETES_NAMESPACE
